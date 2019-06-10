3b22114fbaef91a2edd8d4fba316c931025240cd
Ryzzen <A.mayaud@hotmail.fr>
2019-06-10 15:24:38 +0200

    [UPDATE]: GameObject.hpp

--- src/GameObject/gameObject.hpp
+++ src/GameObject/gameObject.hpp
@@ -6,23 +6,37 @@
 */
 
 #ifndef gameObject
-    #define gameObject
+#define gameObject
 
 #include <vector>
 #include <string>
 
 namespace Game {
 
-    class GameObject {
-    public:
-        std::string _type;
-        std::pair<float, float> _position;
+	enum e_animation {
+		ANIM_IDLE,
+		ANIM_WALKING,
+		ANIM_EXPLODE,
+		ANIM_DYING,
+	};
+
+	class GameObject {
+	public:
+		std::string _type;
+		std::pair<float, float> _position;
 		std::string _meshPath = NULL;
 		std::string _texturePath = NULL;
+		Game::e_animation _animationState = Game::ANIM_IDLE;
 
-		virtual GameObject();
-		virtual ~GameObject();
-    };
+		GameObject(std::string type, std::pair<float, float> position, std::string meshPath, std::string texturePath)
+			: _position(position),
+			_type(type),
+			_meshPath(meshPath),
+			_texturePath(texturePath)
+		{}
+
+		virtual ~GameObject() {}
+	};
 
 }
 
