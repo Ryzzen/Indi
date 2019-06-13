0486310334bb223f398eadb93e982bec9bd84a04
Antoine Mayaud <antoine.mayaud@epitech.eu>
2019-06-13 19:16:39 +0200

    [UPDATE]: fix unsubscription bug

--- src/GameObject/Scene.cpp
+++ src/GameObject/Scene.cpp
@@ -9,88 +9,88 @@
 
 // Global variables
 
-	Game::Scene Game::scene = Game::Scene();
+    Game::Scene Game::scene = Game::Scene();
 
 // Private functions
 
 unsigned int Game::Scene::getUniqueId()
 {
-	std::random_device rd;
-	std::mt19937_64 eng(rd());
-	std::uniform_int_distribution<unsigned int> distr;
-	unsigned int id = distr(eng);
-	 
-	for (id ; _gameObjetcs.count(id) ; id = rand());
-	return (id);
+    std::random_device rd;
+    std::mt19937_64 eng(rd());
+    std::uniform_int_distribution<unsigned int> distr;
+    unsigned int id = distr(eng);
+
+    for (id ; _gameObjetcs.count(id) ; id = rand());
+    return (id);
 }
 
-// Public functions 
+// Public functions
 
 void Game::Scene::moveObject(unsigned int id, float offset_x, float offset_y)
 {
-	if (!_gameObjetcs[id]\
-		|| (_gameObjetcs[id]->_position.first + offset_x < 0 || _gameObjetcs[id]->_position.first + offset_x > _width)\
-		|| (_gameObjetcs[id]->_position.second + offset_y < 0 || _gameObjetcs[id]->_position.second + offset_y > _height))
-		return;
+    if (!_gameObjetcs[id]\
+        || (_gameObjetcs[id]->_position.first + offset_x < 0 || _gameObjetcs[id]->_position.first + offset_x > _width)\
+        || (_gameObjetcs[id]->_position.second + offset_y < 0 || _gameObjetcs[id]->_position.second + offset_y > _height))
+        return;
 
-	_map[static_cast<unsigned int>(_gameObjetcs[id]->_position.second)][static_cast<unsigned int>(_gameObjetcs[id]->_position.first)].removeObject(id);
-	_gameObjetcs[id]->_position.first += offset_x;
-	_gameObjetcs[id]->_position.second += offset_y;
-	_map[static_cast<unsigned int>(_gameObjetcs[id]->_position.second)][static_cast<unsigned int>(_gameObjetcs[id]->_position.first)].addObject(id);
+    _map[static_cast<unsigned int>(_gameObjetcs[id]->_position.second)][static_cast<unsigned int>(_gameObjetcs[id]->_position.first)].removeObject(id);
+    _gameObjetcs[id]->_position.first += offset_x;
+    _gameObjetcs[id]->_position.second += offset_y;
+    _map[static_cast<unsigned int>(_gameObjetcs[id]->_position.second)][static_cast<unsigned int>(_gameObjetcs[id]->_position.first)].addObject(id);
 }
 
 std::vector<unsigned int> Game::Scene::getPosistionObjectsIds(float x, float y) const
 {
-	if ((x < 0 || x > _width)
-		|| (y < 0 || y > _height))
-		return (std::vector<unsigned int>());
+    if ((x < 0 || x > _width)
+        || (y < 0 || y > _height))
+        return (std::vector<unsigned int>());
 
-	std::vector<unsigned int> result;
-	for (unsigned int id : _map[static_cast<unsigned int>(y)][static_cast<unsigned int>(x)]._objects)
-		result.push_back(id);
-	return (result);
+    std::vector<unsigned int> result;
+    for (unsigned int id : _map[static_cast<unsigned int>(y)][static_cast<unsigned int>(x)]._objects)
+        result.push_back(id);
+    return (result);
 }
 
 std::vector<std::string> Game::Scene::getPosistionObjectsTypes(float x, float y)
 {
-	if ((x < 0 || x > _width)
-		|| (y < 0 || y > _height))
-		return (std::vector<std::string>());
+    if ((x < 0 || x > _width)
+        || (y < 0 || y > _height))
+        return (std::vector<std::string>());
 
-	std::vector<std::string> result;
-	for (unsigned int id : _map[static_cast<unsigned int>(y)][static_cast<unsigned int>(x)]._objects)
-		result.push_back(_gameObjetcs[id]->_type);
-	return (result);
+    std::vector<std::string> result;
+    for (unsigned int id : _map[static_cast<unsigned int>(y)][static_cast<unsigned int>(x)]._objects)
+        result.push_back(_gameObjetcs[id]->_type);
+    return (result);
 }
 
 void Game::Scene::update()
 {
-	for (auto const &object : _gameObjetcs)
-		object.second->update();
+    for (auto const &object : _gameObjetcs)
+        object.second->update();
 }
 
 void Game::Scene::takeAction(Game::e_action action)
 {
-	std::function<void()> f = _actionHandler[action];
-	if (!f)
-		return;
-	try
-	{
-		f();
-	}
-	catch (const std::exception&)
-	{
-		return;
-	}
-	
+    std::function<void()> f = _actionHandler[action];
+    if (!f)
+        return;
+    try
+    {
+        f();
+    }
+    catch (const std::exception&)
+    {
+        return;
+    }
+
 }
 
 void Game::Scene::subscribeToAction(Game::e_action action, std::function<void()> f)
 {
-	_actionHandler[action] = f;
+    _actionHandler[action] = f;
 }
 
 void Game::Scene::unsubscribeToAction(Game::e_action action)
 {
-	_actionHandler.erase(action);
+    _actionHandler[action] = NULL;
 }
