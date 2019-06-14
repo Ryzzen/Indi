8d137b02b71c3fbb2dc7bccd56bc833f4fac7226
Baptiste Mayaud <baptiste.mayaud@epitech.eu>
2019-06-14 17:49:05 +0200

    push proto bombs

---
+++ src/GameObject/Bombes/bombes.cpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2019
+** Indi
+** File description:
+** bombes
+*/
+
+#include "bombes.hpp"
+
+Game::Bombe::~Bombe()
+{
+    return;
+}
+
+
+void Game::Bombe::put()
+{
+    return;
+}
+
+void Game::Bombe::_explode()
+{
+    return;
+}
---
+++ src/GameObject/Bombes/bombes.hpp
@@ -0,0 +1,46 @@
+/*
+** EPITECH PROJECT, 2019
+** indi
+** File description:
+** indi
+*/
+
+#ifndef BOMBES
+	#define BOMBES
+
+#include "../GameObject.hpp"
+#include "../Bomberman/Bomberman.hpp"
+
+namespace Game {
+	class Bombe : public Game::GameObject {
+        public:
+                #pragma region publicValues
+
+                #pragma endregion
+
+                #pragma region Constructor / Destructor
+                Bombe(const int radius, const int owner, std::string type, std::string mesh, std::string texture, std::pair<float, float> position = std::make_pair(0.f, 0.f)):
+                        Game::GameObject(type, mesh, texture, position)
+                        {
+                                _radius = radius;
+                                _owner = owner;
+                        }
+                ~Bombe();
+                #pragma endregion
+
+                #pragma region setter / getter
+                const int getRadius() {return (_radius);}
+                void setRadius(const int radius) {_radius = radius;}
+                #pragma endregion
+
+                #pragma region bombFunctions
+                void put();
+                #pragma endregion
+        private:
+                int _radius;
+                int _owner;
+                void _explode();
+	};
+}
+
+#endif /* !BOMBERMAN */
